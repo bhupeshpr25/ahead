188923238f76f805c39eb0039603bde99bd2eee1
Bhupesh <pradhanbhupesh0@gmail.com>
2024-02-15 23:38:14 +0530

    feat: add feature section introduction

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
 import React from "react";
 
-const Features = () => {
-  return <div>Features</div>;
-};
+export default function Features() {
+  return (
+    <div className="bg-red-50 rounded-3xl m-8 mt-4">
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-2 lg:py-32">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
+          <div className="flex">
+            <div className="relative flex items-center gap-x-4 ml-1 rounded-full font-semibold text-gray-900">
+              Built out of frustration
+            </div>
+          </div>
+          <h1 className="mt-5 max-w-lg text-2xl font-bold tracking-tight text-gray-900 lg:text-5xl">
+            Meet the ahead app
+          </h1>
+          <div className="mt-12">
+            <Image src="/features.png" alt="Hero" width={1200} height={1200} />
+          </div>
+        </div>
 
-export default Features;
+        <div className="text-md mt-10 lg:text-2xl lg:mt-0 text-gray-700">
+          <p>
+            A personalized pocket coach that provides bite-sized, science-driven
+            tools that boost emotional intelligence.
+          </p>
+          <br />
+          <p>
+            Think of it as a pocket cheerleader towards a better, more
+            fulfilling living.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
