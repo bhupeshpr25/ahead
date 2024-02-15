56a98bd71e8d7f11662312d1c7fa5fb0a0d92d4d
Bhupesh <pradhanbhupesh0@gmail.com>
2024-02-16 01:33:29 +0530

    feat: add footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,45 @@
+import { EnvelopeIcon, MapPinIcon } from "@heroicons/react/16/solid";
+import Image from "next/image";
 import React from "react";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
-
-export default Footer;
+export default function Footer() {
+  return (
+    <footer className="bg-white">
+      <hr className="border-gray-400 border-t my-10" />
+      <div className="flex flex-col items-center justify-center">
+        <div className="">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={60}
+            height={80}
+            className="rounded-xl"
+          />
+          <div className="text-xl mt-2 font-semibold text-purple-900">
+            ahead
+          </div>
+        </div>
+        <div className="mt-10 justify-center space-x-10 lg:flex">
+          <div className="flex">
+            <div className="bg-green-200 p-2 rounded-full">
+              <MapPinIcon className="h-5 w-5 text-gray-600" />
+            </div>
+            <div className="mt-1 ml-1 font-semibold">
+              Auguststraße 26, 10117 berlin
+            </div>
+          </div>
+          <div className="flex">
+            <div className="bg-green-200 p-2 rounded-full">
+              <EnvelopeIcon className="h-5 w-5 text-gray-600" />
+            </div>
+            <div className=" mt-1 ml-1 font-semibold">hi@ahead-app.com</div>
+          </div>
+        </div>
+        <Image src="/appstore.png" alt="footer" width={300} height={300} />
+        <p className="my-10 text-center text-sm leading-5 text-gray-500">
+          &copy; 2024 Ahead app. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
